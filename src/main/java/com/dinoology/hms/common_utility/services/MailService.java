d1fa6598a1e6c3d4cee87f1b7ddb0e47848cb9d4
Sangeeth Nawarathna <sangeethnawa@gmail.com>
2024-12-10 21:32:55 +0530

    Fix === fixed issues with email

---
+++ src/main/java/com/dinoology/hms/common_utility/services/MailService.java
@@ -0,0 +1,33 @@
+package com.dinoology.hms.common_utility.services;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+/**
+* Author: sangeethnawa
+* Created: 12/10/2024 7:30 PM
+*/
+@Service
+public class MailService {
+
+    private final JavaMailSender mailSender;
+
+    public MailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendHtmlEmail(String to, String subject, String htmlContent) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(htmlContent, true); // true indicates HTML content
+
+        mailSender.send(message);
+    }
+
+}
