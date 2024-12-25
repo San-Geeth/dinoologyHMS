a60ace38e57b581c5baafb2e3fdc02835d764785
Sangeeth Nawarathna <sangeethnawa@gmail.com>
2024-12-25 14:17:16 +0530

    Patient ======= basic modeling for patient

---
+++ src/main/java/com/dinoology/hms/patient/model/Patient.java
@@ -0,0 +1,27 @@
+package com.dinoology.hms.patient.model;
+
+import com.dinoology.hms.common_utility.enums.Title;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*
+ * Created by: sangeethnawa
+ * Date:2024 12/25/2024
+ * Copyright © 2024 DinooLogy
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Patient {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String nic;
+    private String contact;
+    private String gender;
+    @Enumerated(EnumType.STRING)
+    private Title title;
+}
