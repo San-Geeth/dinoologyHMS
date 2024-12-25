220c41f92aacd5bab750da995d48a9af9b55186d
Sangeeth Nawarathna <sangeethnawa@gmail.com>
2024-12-25 14:17:16 +0530

    Patient ======= basic modeling for patient

---
+++ src/main/java/com/dinoology/hms/common_utility/enums/Title.java
@@ -0,0 +1,13 @@
+package com.dinoology.hms.common_utility.enums;
+
+/*
+ * Created by: sangeethnawa
+ * Date:2024 12/25/2024
+ * Copyright © 2024 DinooLogy
+ */
+public enum Title {
+    MR,
+    MRS,
+    MS,
+    MASTER
+}
