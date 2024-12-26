01656d80d18262b83a9a9fbac6f832f5970f4cab
Sangeeth Nawarathna <sangeethnawa@gmail.com>
2024-12-26 20:42:44 +0530

    Models ======= DB models for patient and patient's visit

--- src/main/java/com/dinoology/hms/patient/model/Patient.java
+++ src/main/java/com/dinoology/hms/patient/model/Patient.java
@@ -2,11 +2,15 @@ package com.dinoology.hms.patient.model;
 
 import com.dinoology.hms.common_utility.enums.Gender;
 import com.dinoology.hms.common_utility.enums.Title;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /*
  * Created by: sangeethnawa
@@ -14,16 +18,36 @@ import lombok.NoArgsConstructor;
  * Copyright © 2024 DinooLogy
  */
 @Data
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "patient")
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String firstName;
     private String lastName;
     private String nic;
     private String contact;
+    private String email;
+    private String address;
+
     @Enumerated(EnumType.STRING)
     private Gender gender;
+
     @Enumerated(EnumType.STRING)
     private Title title;
+
+    private LocalDate dob;
+    private Integer age;
+    private String medicalRecordNumber;
+    private LocalDateTime registrationDate;
+    private LocalDateTime lastVisitDate;
+    private Boolean isActive;
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
 }
---
+++ src/main/java/com/dinoology/hms/patient/model/Visit.java
@@ -0,0 +1,42 @@
+package com.dinoology.hms.patient.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/*
+ * Created by: sangeethnawa
+ * Date:2024 12/26/2024
+ * Copyright © 2024 DinooLogy
+ */
+@Entity
+@Table(name = "visit")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Visit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient;
+
+    private LocalDateTime visitDate;
+    private String reasonForVisit;
+    /*TODO: Break Doctor from staff and treat separately.
+    *  Make attendingDoctor data type to Doctor
+    */
+    private String attendingDoctor;
+    private String visitNotes;
+    /*TODO: Create separate service for services
+     *  that can add services as hospital need
+     */
+    private String service;
+}
