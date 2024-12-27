5742f8e97a6a368543f4bb616342287ef6c2c6b7
Sangeeth Nawarathna <63340199+San-Geeth@users.noreply.github.com>
2024-12-27 20:23:18 +0530

    Feature/patient (#33)
    * Patient
    =======
    basic modeling for patient
    
    * Fix
    =======
    changed gender data type to ENum created
    
    * Models
    =======
    DB models for patient and patient's visit
    
    * Patient
    =======
    basic modeling for patient
    
    * Fix
    =======
    changed gender data type to ENum created
    
    * Models
    =======
    DB models for patient and patient's visit
    
    * Service
    =======
    new patient add service
    
    * Service
    =======
    calculated age from bday

---
+++ src/main/java/com/dinoology/hms/patient/model/Patient.java
@@ -0,0 +1,47 @@
+package com.dinoology.hms.patient.model;
+
+import com.dinoology.hms.common_utility.enums.Gender;
+import com.dinoology.hms.common_utility.enums.Title;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/*
+ * Created by: sangeethnawa
+ * Date:2024 12/25/2024
+ * Copyright © 2024 DinooLogy
+ */
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "patient")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String pid;
+    private String firstName;
+    private String lastName;
+    private String nic;
+    private Long contact;
+    private String email;
+    private String address;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    @Enumerated(EnumType.STRING)
+    private Title title;
+    private LocalDate dob;
+    private Integer age;
+    private Boolean isActive = true;
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+}
