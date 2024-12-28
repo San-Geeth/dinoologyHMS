c3e6e4b4c6b034bc98f591a95f730474d3823228
Sangeeth Nawarathna <sangeethnawa@gmail.com>
2024-12-28 23:18:29 +0530

    Doctor ======= Basic model for doctor

---
+++ src/main/java/com/dinoology/hms/staff/model/Doctor.java
@@ -0,0 +1,49 @@
+package com.dinoology.hms.staff.model;
+
+import com.dinoology.hms.common_utility.enums.Gender;
+import com.dinoology.hms.common_utility.enums.MaritalStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+/*
+ * Created by: sangeethnawa
+ * Date:2024 12/28/2024
+ * Copyright © 2024 DinooLogy
+ */
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String profileName;
+    private String education;
+    private String appointedHospital;
+    private String nic;
+    private String email;
+    private String primaryContact;
+    private String secondaryContact;
+    private String address;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    @Enumerated(EnumType.STRING)
+    private MaritalStatus maritalStatus;
+    private Integer rate;
+    private String did;
+    private String emergencyContact;
+    private String emergencyName;
+    private Boolean activeStatus = true;
+    private String imgURL;
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+}
